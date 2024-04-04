32b62b4b45b64eedb782cc5dba51c4bd493703a2
Hsungjin <hsungjin142@gmail.com>
2024-04-04 16:28:18 +0900

    Flutter 기초 구조, 위젯 학습

---
+++ toonapp/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          elevation: 99,
+          title: Text("Hello Flutter!"),
+        ),
+        body: Center(
+          child: Text('Hello world!'),
+        ),
+      ),
+    );
+  }
+
+}
