00e2c42beaa05422ab14f363b4dffe4a5686b15f
Hsungjin <hsungjin142@gmail.com>
2024-09-18 16:52:13 +0900

    토스 UI -  혜택 강의 내용

--- fastcampus/ttoss/lib/common/theme/color/abs_theme_colors.dart
+++ fastcampus/ttoss/lib/common/theme/color/abs_theme_colors.dart
@@ -51,4 +51,6 @@ abstract class AbstractThemeColors {
   Color get buttonBackground => const Color.fromARGB(255, 48, 48, 48);
 
   Color get roundedLayouyBackground => const Color.fromARGB(255, 24, 24, 24);
+
+  Color get blueText => AppColors.blue;
 }
