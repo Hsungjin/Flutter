5734e494ce13db3de0d33602d6a54bb1f78b3054
Hsungjin <120264964+Hsungjin@users.noreply.github.com>
2024-10-22 20:01:09 +0900

    todo앱 관련 강의 내용 추가

---
+++ fastcampus/todo/lib/common/data/memory/todo_data_norifier.dart
@@ -0,0 +1,11 @@
+import 'package:fast_app_base/common/data/memory/vo_todo.dart';
+import 'package:flutter/cupertino.dart';
+
+class TodoDataNotifier extends ValueNotifier<List<Todo>> {
+  TodoDataNotifier(): super([]);
+
+  void addTodo(Todo todo) {
+    value.add(todo);
+    notifyListeners();
+  }
+}
---
+++ fastcampus/todo/lib/common/widget/constant_widget.dart
@@ -0,0 +1,12 @@
+import 'package:fast_app_base/common/widget/w_height_and_width.dart';
+import 'package:flutter/cupertino.dart';
+
+const spacer = Spacer();
+
+const height10 = Height(10);
+const height15 = Height(15);
+const height20 = Height(20);
+
+const widtht10 = Width(10);
+const widtht15 = Width(15);
+const widtht20 = Width(20);
