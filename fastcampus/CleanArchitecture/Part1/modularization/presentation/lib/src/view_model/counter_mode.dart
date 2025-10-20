3c34bb6a9188e78d5ae9e481af8b16af106758f5
Hsungjin <hsungjin142@gmail.com>
2025-10-20 16:53:39 +0900

    [패캠] 모듈화

---
+++ fastcampus/CleanArchitecture/Part1/modularization/presentation/lib/src/view_model/counter_mode.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/foundation.dart';
+import 'package:modularization_domain/domain.dart';
+
+class CounterModeViewModel {
+  final CounterModeModel _counterModeModel;
+
+  CounterModeViewModel(this._counterModeModel);
+
+  final ValueNotifier<CounterMode> _counterMode = ValueNotifier<CounterMode>(
+    CounterMode.plus,
+  );
+
+  ValueNotifier<CounterMode> get counterMode => _counterMode;
+
+  void toggleMode() {
+    _counterModeModel.toggleMode();
+    _update();
+  }
+
+  void _update() {
+    _counterMode.value = _counterModeModel.counterMode;
+  }
+}
