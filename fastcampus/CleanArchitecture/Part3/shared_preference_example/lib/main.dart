1e5ef7cfbd58b45ebc290025fca6bf31a6f6c94a
Hsungjin <hsungjin142@gmail.com>
2025-10-27 19:37:51 +0900

    [패캠] SharedPreferences 학습

---
+++ fastcampus/CleanArchitecture/Part3/shared_preference_example/lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+final sharedPreferencesProvider = Provider<SharedPreferences>((ref) {
+  throw UnimplementedError();
+});
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final sharedPreferences = await SharedPreferences.getInstance();
+
+  runApp(
+    ProviderScope(
+      overrides: [
+        sharedPreferencesProvider.overrideWithValue(sharedPreferences),
+      ],
+      child: MainApp(),
+    ),
+  );
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Consumer(
+            builder: (context, ref, child) {
+              final sharedPreferences = ref.watch(sharedPreferencesProvider);
+              return Text(sharedPreferences.getString('name') ?? 'No name');
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
